352a776805dfcc722b58b1cf84e28f6c879524ef
MelanieXinyi <melanie.xinyi.porter@gmail.com>
2026-03-18 16:48:45 -0700

    window with square so cool

--- src/conwaygame/Main.java
+++ src/conwaygame/Main.java
@@ -1,8 +1,26 @@
 package conwaygame;
 
-public class Main {
+import javax.swing.*;
 
+public class Main {
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Conway's Game of Life");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+
         System.out.println("Hello world");
     }
+
+
+
 }
